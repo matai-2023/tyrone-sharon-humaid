6351ac5b3c45b8194a82e8b1d54134a8c6e4ff93
Sharon-Li <136324075+Sharon-Li-77@users.noreply.github.com>
2023-08-04 10:54:12 +1200

    server.post

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import * as Path from 'node:path'
 // import * as URL from 'node:url'
 
-import express from 'express'
+import express, { json } from 'express'
 import hbs from 'express-handlebars'
+import fsPromises from 'node:fs/promises'
 
 const server = express()
 
@@ -18,8 +19,15 @@ server.set('views', Path.resolve('server/views'))
 
 // Your routes/router(s) should go here
 
-server.get('/', (req, res) => {
-  res.render('home')
+server.post('/', async (req, res) => {
+  const body = req.body
+  const filePath = Path.join(__dirname, 'data', 'data.jaosn')
+  bodyArray = JSON.parse(body)
+  let newData = bodyArray.map
+
+  const data = await fsPromises.writeFile(filePath)
+  const ViewData = JSON.parse(data)
+  res.render('home', ViewData)
 })
 
 export default server
